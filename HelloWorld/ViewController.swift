e6f879b881c5c3b3a93f1dce687a805cd661be73
Nancy McNamara <nmcnama1@nd.edu>
2016-11-07 01:42:35 -0500

    map image and UI test interactivity

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var topLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,9 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        topLabel.text=textField.text
+    }
+    
+    //MARK: Actions
     @IBAction func showMessage() {
         let alertController = UIAlertController(title: "Welcome", message: "Hello world!", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func changeLabelText(_ sender: UIButton) {
+        topLabel.text = "tester"
     }
 }
